4ab77ea48d2609dd0e05ca789720d33bfb29a9e9
tjamescpp <ttjames@cpp.edu>
2024-11-20 13:47:02 -0800

    fixed team logos

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def display_games():
 
         # get team logo
         away_team_logo = get_team_logo(away_team_name)
+        home_team_logo = get_team_logo(home_team_name)
 
         # get team scores
         home_score = pts_data.loc[pts_data['TEAM_ID']
@@ -77,6 +78,7 @@ def display_games():
             "away_score": away_score,
             "home_score": home_score,
             "away_team_logo": away_team_logo,
+            "home_team_logo": home_team_logo,
             "game_time": game_time_ltz.strftime('%Y-%m-%d'),
             "game_status": game_status
         })
@@ -109,7 +111,36 @@ def generate_recap(game_id):
 def get_team_logo(team_name):
 
     team_logos = {
-        "Lakers": "https://content.sportslogos.net/logos/6/237/full/los_angeles_lakers_logo_primary_2024_sportslogosnet-7324.png"
+        "Hawks": "https://cdn.nba.com/logos/nba/1610612737/primary/L/logo.svg",
+        "Celtics": "https://cdn.nba.com/logos/nba/1610612738/primary/L/logo.svg",
+        "Nets": "https://cdn.nba.com/logos/nba/1610612751/primary/L/logo.svg",
+        "Hornets": "https://cdn.nba.com/logos/nba/1610612766/primary/L/logo.svg",
+        "Bulls": "https://cdn.nba.com/logos/nba/1610612741/primary/L/logo.svg",
+        "Cavaliers": "https://cdn.nba.com/logos/nba/1610612739/primary/L/logo.svg",
+        "Mavericks": "https://cdn.nba.com/logos/nba/1610612742/primary/L/logo.svg",
+        "Nuggets": "https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg",
+        "Pistons": "https://cdn.nba.com/logos/nba/1610612765/primary/L/logo.svg",
+        "Warriors": "https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg",
+        "Rockets": "https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg",
+        "Pacers": "https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg",
+        "Clippers": "https://cdn.nba.com/logos/nba/1610612746/primary/L/logo.svg",
+        "Lakers": "https://cdn.nba.com/logos/nba/1610612747/primary/L/logo.svg",
+        "Grizzlies": "https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg",
+        "Heat": "https://cdn.nba.com/logos/nba/1610612748/primary/L/logo.svg",
+        "Bucks": "https://cdn.nba.com/logos/nba/1610612749/primary/L/logo.svg",
+        "Timberwolves": "https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg",
+        "Pelicans": "https://cdn.nba.com/logos/nba/1610612740/primary/L/logo.svg",
+        "Knicks": "https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg",
+        "Thunder": "https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg",
+        "Magic": "https://cdn.nba.com/logos/nba/1610612753/primary/L/logo.svg",
+        "76ers": "https://cdn.nba.com/logos/nba/1610612755/primary/L/logo.svg",
+        "Suns": "https://cdn.nba.com/logos/nba/1610612756/primary/L/logo.svg",
+        "Trail Blazers": "https://cdn.nba.com/logos/nba/1610612757/primary/L/logo.svg",
+        "Kings": "https://cdn.nba.com/logos/nba/1610612758/primary/L/logo.svg",
+        "Spurs": "https://cdn.nba.com/logos/nba/1610612759/primary/L/logo.svg",
+        "Raptors": "https://cdn.nba.com/logos/nba/1610612761/primary/L/logo.svg",
+        "Jazz": "https://cdn.nba.com/logos/nba/1610612762/primary/L/logo.svg",
+        "Wizards": "https://cdn.nba.com/logos/nba/1610612764/primary/L/logo.svg"
         # Add all other teams
     }
 
